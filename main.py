a74dc275abeb1802e5966c6057b9e55d2aa21348
Dave Riedstra <dave.riedstra@gmail.com>
2019-02-09 19:22:00 -0500

    implement python-evdev async API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import asyncio
 from evdev import ecodes as e, InputDevice, UInput, categorize
 
 # NTS - if opening /dev/uinput doesn't work,
@@ -9,44 +10,48 @@ from evdev import ecodes as e, InputDevice, UInput, categorize
 dev_name = '/dev/input/event14'
 
 device = InputDevice(dev_name)
-ui = UInput.from_device(device, name=device.name, version=3)
+surrogate = UInput.from_device(device, name=device.name, version=3)
 
-dragging = False
 
-# todo - try out the async API
-for event in device.read_loop():
-    if event.type == e.EV_KEY:
-        event = categorize(event)
-        if event.keycode == 'BTN_TOOL_TRIPLETAP':
-            dragging = event.keystate == 1
-            click_val = 1 if dragging else 0
+async def handler(dev, sur):
+    dragging = False
+    async for event in dev.async_read_loop():
+        if event.type == e.EV_KEY:
+            event = categorize(event)
+            if event.keycode == 'BTN_TOOL_TRIPLETAP':
+                dragging = event.keystate == 1
+                click_val = 1 if dragging else 0
 
-            # to fake multitouch events, even if it's just one touch,
-            # we need to fake these events as well.
-            # I'm not sure where ID comes from (except that it's -1
-            # when the touch leaves), but my birth year happens to be in range
-            ui.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 1989 * click_val - 1)
-            ui.write(e.EV_KEY, e.BTN_TOUCH, click_val)
-            ui.write(e.EV_KEY, e.BTN_TOOL_FINGER, click_val)
+                # to fake multitouch events, even if it's just one touch,
+                # we need to fake these events as well.
+                # I'm not sure where ID comes from (except that it's -1
+                # when the touch leaves), but my birth year happens to be valid
+                sur.write(e.EV_ABS, e.ABS_MT_TRACKING_ID, 1989 * click_val - 1)
+                sur.write(e.EV_KEY, e.BTN_TOUCH, click_val)
+                sur.write(e.EV_KEY, e.BTN_TOOL_FINGER, click_val)
 
-            # this is the actual click event
-            ui.write(e.EV_KEY, e.BTN_LEFT, click_val)
-            ui.syn()
+                # this is the actual click event
+                sur.write(e.EV_KEY, e.BTN_LEFT, click_val)
+                sur.syn()
 
-            # idea: temporarily grab the device to avoid passing on events
-            # to the DE (handy for GNOME on Wayland)
-            # if dragging:
-            #     device.grab()
-            # else:
-            #     device.ungrab()
+                # idea: temporarily grab the device to avoid passing on events
+                # to the DE (handy for GNOME on Wayland)
+                # if dragging:
+                #     dev.grab()
+                # else:
+                #     dev.ungrab()
 
-    elif dragging:
-        if event.type == e.EV_ABS:
-            if event.code == e.ABS_X:
-                ui.write(e.EV_ABS, e.ABS_MT_POSITION_X, event.value)
-            elif event.code == e.ABS_Y:
-                ui.write(e.EV_ABS, e.ABS_MT_POSITION_Y, event.value)
-        elif event.type == e.EV_SYN:
-            ui.syn()
+        elif dragging:
+            if event.type == e.EV_ABS:
+                if event.code == e.ABS_X:
+                    sur.write(e.EV_ABS, e.ABS_MT_POSITION_X, event.value)
+                elif event.code == e.ABS_Y:
+                    sur.write(e.EV_ABS, e.ABS_MT_POSITION_Y, event.value)
+            elif event.type == e.EV_SYN:
+                sur.syn()
 
-ui.close()
+    sur.close()
+
+# start handling async events
+loop = asyncio.get_event_loop()
+loop.run_until_complete(handler(device, surrogate))
