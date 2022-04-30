bcdf2af083a492d896c22e4ecfed8de584b28a95
Dave Riedstra <dave.riedstra@gmail.com>
2022-04-30 23:00:05 +0100

    don't require BTN_LEFT as we can now fake it properly

--- get_gesture_devices.py
+++ get_gesture_devices.py
@@ -15,8 +15,7 @@ def get_gesture_devices(num_fingers = 3):
         finger_key = ec.BTN_TOOL_TRIPLETAP if num_fingers == 3 else ec.BTN_TOOL_QUADTAP
         has_key = (ec.EV_KEY in caps and finger_key in caps[ec.EV_KEY]
             and ec.BTN_TOOL_FINGER in caps[ec.EV_KEY]
-            and ec.BTN_TOUCH in caps[ec.EV_KEY]
-            and ec.BTN_LEFT in caps[ec.EV_KEY])
+            and ec.BTN_TOUCH in caps[ec.EV_KEY])
 
         return has_key
 
